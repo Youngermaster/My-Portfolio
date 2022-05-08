b0922d635c799d996975cc552e08f1db2eacb0d7
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2022-05-08 13:18:53 -0500

    Skills and Work experience migrated from Sanity to Local

---
+++ src/constants/skills.js
@@ -0,0 +1,13 @@
+import { images } from '.';
+
+const skills = [
+  { name: 'Flutter', bgColor: '#ffffff', image: images.flutter },
+  { name: 'Python', bgColor: '#ffffff', image: images.python },
+  { name: 'Flutter', bgColor: '#ffffff', image: images.cpp },
+  { name: 'Flutter', bgColor: '#ffffff', image: images.cpp },
+  { name: 'Flutter', bgColor: '#ffffff', image: images.cpp },
+  { name: 'Flutter', bgColor: '#ffffff', image: images.cpp },
+  { name: 'Flutter', bgColor: '#ffffff', image: images.cpp },
+];
+
+export default skills;
---
+++ src/constants/workExperiences.js
@@ -0,0 +1,11 @@
+const workExperiences = [
+  { name: 'Student', company: 'EAFIT', desc: 'I Started my journey, here I learned the basics of programming and version control systems (subversion and git). I worked with languages like Java, C++, Haskell, Ruby, C#, PHP.' },
+  { name: 'Student - Independent tutor', company: 'EAFIT', desc: 'I learned more in depth of how things worked internally, then I learned about software development best practices, Full Stack development and some Flutter. And also I started to teach to several students about programming related topics.' },
+  { name: 'Developer', company: 'Independent', desc: "I improved my coding skills, I published 2 APKs to the Play Store, and I was on two 'University Clubs', The Software Development Club and the EAFIT Condor Hyperloop (Yup, we were working to participate in the Elon Musk's competition)." },
+  { name: 'Software Quality Assurance Tester', company: 'GETA Club', desc: 'I examined software applications (Unity 3D, Angular and .NET Core) to assess whether or not they worked as expected. I also ran automated tests with Selenium.' },
+  { name: 'Cyber Security Analyst', company: 'GETA Club', desc: 'I Helped to protect the company by developing a variety of technologies and processes to prevent, detect, and manage cyber threats. As a cyber security analyst, I have also worked with ISO 27001, SAST and DAST Technologies.' },
+  { name: 'IOT Developer', company: 'Della Smart Glasses', desc: 'I tried to Co-Create a company focused on the manufacturing of Smart AR Glasses.' },
+  { name: 'Cyber Security Analyst', company: 'GETA Club', desc: 'd' },
+];
+
+export default workExperiences;
