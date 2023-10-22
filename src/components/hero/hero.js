a986ccea119ca3889a0432ba687725e200a5f2f5
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2023-10-22 18:35:24 -0500

    Hero and banner finished

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -4,47 +4,47 @@ import himg from '../../images/Younger.svg'
 import { Link } from 'react-scroll'
 
 const Hero = () => {
-    return (
-        <section className="tp-hero-section-1">
-            <div className="container">
-                <div className="row">
-                    <div className="col col-xs-7 col-lg-7">
-                        <div className="tp-hero-section-text">
-                            <div className="tp-hero-title">
-                                <h2>Problem <br /> Solver</h2>
-                            </div>
-                            <div className="tp-hero-sub">
-                                <p><span>J</span>uan <span>M</span>anuel <span>Y</span>oung <span>H</span>oyos</p>
-                            </div>
-                            <div className="btns">
-                                <Link activeClass="active" to="contact" spy={true} smooth={true} duration={500} offset={-95} className="theme-btn">Contact Me</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section className="tp-hero-section-1">
+      <div className="container">
+        <div className="row">
+          <div className="col col-xs-7 col-lg-7">
+            <div className="tp-hero-section-text">
+              <div className="tp-hero-title">
+                <h2>Problem <br /> Solver</h2>
+              </div>
+              <div className="tp-hero-sub">
+                <p><span>J</span>uan <span>M</span>anuel <span>Y</span>oung <span>H</span>oyos</p>
+              </div>
+              <div className="btns">
+                <Link activeClass="active" to="contact" spy={true} smooth={true} duration={500} offset={-95} className="theme-btn">Contact Me</Link>
+              </div>
             </div>
-            <div className="right-vec">
-                <div className="right-img">
-                    <img src={himg} alt="" />
-                </div>
-            </div>
-            <div className="social-link">
-                <ul>
-                    <li><a href="https://github.com/Youngermaster">GitHub</a></li>
-                    <li><a href="https://www.linkedin.com/in/juan-manuel-young-hoyos/">LinkedIn</a></li>
-                    <li><a href="https://www.instagram.com/jmyounghoyos/">Instagram</a></li>
-                </ul>
-            </div>
-            <div className="visible-text">
-                <h1>
-                    APPS <br />
-                    AI <br />
-                    SECURITY <br />
-                    DEVOPS
-                </h1>
-            </div>
-        </section>
-    )
+          </div>
+        </div>
+      </div>
+      <div className="right-vec">
+        <div className="right-img">
+          <img src={himg} alt="" />
+        </div>
+      </div>
+      <div className="social-link">
+        <ul>
+          <li><a href="https://github.com/Youngermaster">GitHub</a></li>
+          <li><a href="https://www.linkedin.com/in/juan-manuel-young-hoyos/">LinkedIn</a></li>
+          <li><a href="https://www.instagram.com/jmyounghoyos/">Instagram</a></li>
+        </ul>
+      </div>
+      <div className="visible-text">
+        <h1>
+          DEVSECOPS  <br />
+          APPS <br />
+          AI <br />
+          IOT
+        </h1>
+      </div>
+    </section>
+  )
 }
 
 export default Hero;
--- src/main-component/router/index.js
+++ src/main-component/router/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import {BrowserRouter,Routes,Route,} from "react-router-dom";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 
 import Homepage from '../HomePage/HomePage'
 
-const AllRoute = () => { 
+const AllRoute = () => {
 
-  return (    <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="home" element={<Homepage />} />
-          </Routes>
-        </BrowserRouter>
-      
-    </div>
+  return (<div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="home" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
+
+  </div>
   );
 }
 
