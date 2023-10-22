9ab6dd6c1a7047809bf619c658231e83ccbf3427
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2023-10-22 18:58:58 -0500

    Marquee finished

--- src/components/marque/marque.js
+++ src/components/marque/marque.js
@@ -1,27 +1,27 @@
 import React from 'react'
 
 const Marquee = (props) => {
-    return (
-        <div className="digital-marque-sec">
-            <div className="container">
-                <div className="digital-marque">
-                    <div className="marquee">
-                        <div className="track">
-                            <div className="content">
-                                <h1>
-                                    <span>I always try to put my best service to my client</span>
-                                    <i>.</i> <span>I always try to put my best service to my client</span>
-                                    <i>.</i> <span>I always try to put my best service to my client</span>
-                                    <i>.</i> <span>I always try to put my best service to my client</span>
-                                    <i>.</i> <span>I always try to put my best service to my client</span> <i>.</i>
-                                </h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="digital-marque-sec">
+      <div className="container">
+        <div className="digital-marque">
+          <div className="marquee">
+            <div className="track">
+              <div className="content">
+                <h1>
+                  <span>Committed to Delivering Excellence</span>
+                  <i>.</i> <span>Driven by Quality, Guided by Service</span>
+                  <i>.</i> <span>Striving for the Best for Every Client</span>
+                  <i>.</i> <span>Your Success, My Priority</span>
+                  <i>.</i> <span>Excellence is My Signature</span> <i>.</i>
+                </h1>
+              </div>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Marquee;
