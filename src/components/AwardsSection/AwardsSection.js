4997843acc384296166fa9fb3e6185ca6629f95c
Juan Manuel Young Hoyos <vndrzrfshJXY@wegmans.com>
2024-04-25 01:14:53 -0500

    Refactor awards.js and AwardsSection.js components

--- src/components/AwardsSection/AwardsSection.js
+++ src/components/AwardsSection/AwardsSection.js
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import awards from '../../api/awards'
+import awards from "../../api/awards";
 import AwardSingle from "../AwardSingle";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const AwardsSection = () => {
-
   const [open, setOpen] = React.useState(false);
 
   function handleClose() {
     setOpen(false);
   }
 
-  const [state, setState] = useState({})
+  const [state, setState] = useState({});
 
   const handleClickOpen = (item) => {
     setOpen(true);
-    setState(item)
-  }
+    setState(item);
+  };
 
   const [number, setCount] = useState(3);
   const [buttonActive, setButtonState] = useState(false);
@@ -33,7 +32,10 @@ const AwardsSection = () => {
           <div className="row">
             {awards.slice(0, number).map((blog, bl) => (
               <div className="col col-lg-4 col-md-6 col-12" key={bl}>
-                <div className="tp-blog-item" onClick={() => handleClickOpen(blog)}>
+                <div
+                  className="tp-blog-item"
+                  onClick={() => handleClickOpen(blog)}
+                >
                   <div className="tp-blog-img">
                     <img src={blog.screens} alt="" />
                   </div>
@@ -42,16 +44,23 @@ const AwardsSection = () => {
                       <li>{blog.create_at}</li>
                       <li>At {blog.location}</li>
                     </ul>
-                    <h2 >{blog.title}</h2>
-                    <p>{blog.description.substring(0, 150) + '...'}</p>
+                    <h2>{blog.title}</h2>
+                    <p>{blog.description.substring(0, 150) + "..."}</p>
                   </div>
                 </div>
               </div>
             ))}
           </div>
-          <div className={`sec-title-btn text-center mt-3 ${buttonActive ? "d-none" : ""}`}>
+          <div
+            className={`sec-title-btn text-center mt-3 ${
+              buttonActive ? "d-none" : ""
+            }`}
+          >
             <span onClick={() => setButtonState(!buttonActive)}>
-              <button className="theme-btn" onClick={() => setCount(number + number)}>
+              <button
+                className="theme-btn"
+                onClick={() => setCount(number + number)}
+              >
                 View All Achievements
               </button>
             </span>
@@ -61,9 +70,18 @@ const AwardsSection = () => {
       <div className="visible-rotate-text">
         <h1>My Latest Awards</h1>
       </div>
-      <AwardSingle open={open} onClose={handleClose} title={state.title} bImg={state.blogSingleImg} create_at={state.create_at} author={state.author} comment={state.comment} description={state.description} />
+      <AwardSingle
+        open={open}
+        onClose={handleClose}
+        title={state.title}
+        bImg={state.blogSingleImg}
+        create_at={state.create_at}
+        author={state.author}
+        comment={state.comment}
+        description={state.description}
+      />
     </section>
   );
-}
+};
 
 export default AwardsSection;
